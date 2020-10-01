c8dd4d7d9d9991cc66a9f61689d1e3e4a1a467a2
Thibaut-Arnoux <thib.arnoux@gmail.com>
2020-10-01 09:52:24 +0200

    Add dependency and update initial project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import Auth from './components/Auth';
+import Board from './components/Board';
+import Dashboard from './components/Dashboard'
+import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    // <div className="App">
+      <BrowserRouter>
+        <Route exact path="/" component={Auth} />
+        <Route path="/board" component={Board} />
+        <Route path="/dashboard/:boardId" component={Dashboard} />
       
-    </div>
+      </BrowserRouter>
+    // </div>
   );
 }
 
