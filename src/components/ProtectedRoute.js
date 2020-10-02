3fee165782d9806524e01b85bdf22919d4f711d8
Thibaut-Arnoux <thib.arnoux@gmail.com>
2020-10-02 17:35:15 +0200

    Add Protected routes

---
+++ src/components/ProtectedRoute.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import { Redirect, Route } from 'react-router-dom'
+
+class ProtectedRoute extends Component {
+    render() {
+        const Component = this.props.component;
+        console.log(this.props)
+        const path = this.props.path
+        const isAuthenticated = localStorage.getItem('access_token')
+
+        return isAuthenticated ? (
+            <Route exact path={path} component={Component} />
+        ) : 
+        (
+            <Redirect to='/' />
+        );
+    }
+}
+
+export default ProtectedRoute;
