1d0925e8e901404d9197b116d96a30f21b693807
Thibaut-Arnoux <thib.arnoux@gmail.com>
2020-10-04 18:38:05 +0200

    Update Table with bootstrap

--- src/components/Part.js
+++ src/components/Part.js
@@ -1,25 +1,28 @@
 import React from 'react'
+import { Table } from 'react-bootstrap'
+
+// <table style={{float: 'left'}}>
 
 function Part({part}){
     return(
-            <table style={{float: 'left'}}>
-                <thead>
-                    <tr>
-                        <th> {part.name} {part.tag} </th>
-                    </tr>
-                </thead>
-                <tbody>                                    
-                    {
-                        part.msgs.map((msg, index) => {
-                            return(
-                                <tr key={index}>
-                                    <td>{msg} </td>
-                                </tr>
-                            ) 
-                        })
-                    }                                       
-                </tbody>
-            </table>
+        <Table striped bordered hover>
+            <thead>
+                <tr>
+                    <th> {part.name} {part.tag} </th>
+                </tr>
+            </thead>
+            <tbody>                                    
+                {
+                    part.msgs.map((msg, index) => {
+                        return(
+                            <tr key={index}>
+                                <td>{msg} </td>
+                            </tr>
+                        ) 
+                    })
+                }                                       
+            </tbody>
+        </Table>
     );
 }
 
